7394bd20470222c4a429a6d0779bfa68a18bafca
faisal <faisalnkpadi@gmail.com>
2025-07-23 13:00:36 -0400

    Changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,12 @@
       <div style="background: rgba(0, 0, 0, 0.6); height: 100%">
         <div class="d-flex align-center flex-row justify-center fill-height">
           <div class="d-flex flex-column fill-height justify-center align-center text-white">
-            <h1 class="text-h2 font-weight-bold mb-4 poppins-bold text-center">
+            <!-- <h1 class="text-h2 font-weight-bold mb-4 poppins-bold text-center">
               Sulthan Pages
-            </h1>
+            </h1> -->
+            <div class="logo-wrapper">
+              <img class="logo" src="/logo.png" style="width: 20em; ">
+            </div>
             <div class="typewriter pl-4">
               <h3 class="subheading ">
                 "A Call to Thawheed"
@@ -20,22 +23,21 @@
 
     <div class="image">
       <Transition name="slide-fade">
-        <img v-if="showImage" src="/mahan.png" />
+        <img v-if="showImage" src="/mahan.png">
       </Transition>
     </div>
   </div>
 
-
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+  import { onMounted, ref } from 'vue'
 
-const showImage = ref(false)
+  const showImage = ref(false)
 
-onMounted(() => {
-  showImage.value = true
-})
+  onMounted(() => {
+    showImage.value = true
+  })
 </script>
 <style scoped>
 .typewriter .subheading {
@@ -118,7 +120,6 @@ onMounted(() => {
   }
 }
 
-
 @media (max-width: 1320px) and (min-width: 600px) {
   .image {
     right: -80px;
@@ -135,5 +136,13 @@ onMounted(() => {
     bottom: -168px;
     right: 100px;
   }
+}
+
+.logo-wrapper {
+  /* background-color: white; */
+}
+
+.logo {
+  filter: brightness(0) invert(1);
 }
 </style>
