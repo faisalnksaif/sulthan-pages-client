3af6752769704131eb862686fd589c0680773c23
Faisal <faisalnkpadi@gmail.com>
2025-08-30 20:58:36 -0400

    Updates

---
+++ _nuxt/LbM4Ij1N.js
@@ -0,0 +1,37 @@
+const e={mission:{title:"My Mission is the Call to Tawheed",quote:`
+    The noble declaration <b>La ilaha illallah, Muhammadur Rasulullah</b> was the divine message upheld by
+      every
+      Prophet appointed to this world. Each one of them bore witness to this truth, and every bearer of this
+      sacred
+      message endured trials and tribulations along their path.
+    `,author:"Quthubuzzaman Sheikh Yusuf Sulthan Shah Qadiri Chishti",authorImage:"./mahan.jpg",background:"./home.jpg",article:`
+    The core of my mission is the invitation to Tawheed — the oneness of Allah and the affirmation that Prophet Muhammed is His Messenger. The noble declaration "Lā ilāha illallāh, Muhammadur Rasūlullāh" was the divine message upheld by every Prophet appointed to this world. Each one of them bore witness to this truth, and every bearer of this sacred message endured trials and tribulations along their path.<br> <br>
+    It is through this lens that we understand the profound saying of the Prophet Muhammad (ﷺ):<br>
+    "The most severely tested people are the Prophets, then the righteous, and then those who resemble them."<br>
+    Indeed, our beloved Prophet (ﷺ), his companions, and Ghaus al-A‘zam all endured immense hardships — in their efforts to fulfil this mission. But they persevered, they prevailed.<br><br>
+    The call to Tawheed, is not a slogan to be raised nor a banner for outward reform. It is the call to establish the Tawheed deep within the soul — to live in constant awareness of His presence, never allowing the heart to slip into heedlessness. True nearness to the Almighty is only attained through this continuous remembrance. When the heart is emptied of Tawheed, it becomes exposed and vulnerable — and that is when Iblis finds his path inward, stirring our thoughts, influencing our actions and tempting to follow corrupt desires. Our call, therefore, is not merely to speak of Tawheed, but to embody it — so that every inward state and outward deed is shaped by its light.<br><br>
+    After me, it is the time of Imam Mahdi (عليه السلام). He too will face trials. Among his fiercest opponents will be the corrupt scholars of the time. However, those who have entered the spiritual chain (silsila) will, through the guidance of their Shaikhs, recognize the truth of Mahdi (عليه السلام) and will ultimately become his followers.<br><br>
+    Those who oppose Mahdi (عليه السلام) are not the true scholars of Islam — just as today's corrupt scholars who oppose the pure message of Tawheed and its callers. This is because the hadith: "The scholars of my Ummah are like the Prophets of the Children of Israel", and similar hadiths that praise scholars, refer only to those who have attained ‘Ilm al-Ma‘rifah (knowledge of divine realities) and ‘Ilm al-Yaqīn (certainty in faith) — not those who limit themselves to outward rulings and legalities while neglecting the deeper work of purifying the heart and enlightening the soul.<br><br>
+    The opposition of corrupt scholars, their power, and their wealth pose no threat to us. Our reliance is upon Allah alone. Had power, wealth, and empty scholarship been the ultimate measure of success, then Pharaoh, Qarun, Balaam, Ibn Sakha, and Jalut would have triumphed. But they did not. Instead, victory belonged to Musa (عليه السلام) who was raised in Pharaoh’s own palace, to Ghaus al-A‘zam who honored the Auliya, and to Talut who was chosen by Allah.<br><br>
+    I did not begin this mission of calling to Tawheed on my own. I received divine permission to do so. And I now remind you: if ego-driven stubbornness, attachment to titles and status, entrapment within institutional frameworks, preoccupation with follower count, or fear of losing wealth and material comforts hold you back from embracing this sacred path — then know that you are heading toward utter failure in the sight of Allah.<br><br>
+  `},voiceOfKhalifa:{title:"Worship without Tawheed is a Soulless Deed",quote:`
+      No action we perform can be deemed worship unless it is executed in accordance with the sacred ordinances of
+      Allah. The Almighty has instructed us to worship him in a way that draws us near to Him, with no veils creating
+      distance or detachment. <br>The closeness can only be attained when the pure light of Tawheed is engraved deeply
+      within
+      the soul.
+    `,author:`
+    Sheikh Abbas Faizy Vazhikkadav<br>
+    Khalifa of Quthubuzzaman
+    `,authorImage:"./usthad.png",article:`
+    Allah, the Exalted, created mankind and jinn solely to worship Him. Worship, in its truest essence, is the profound manifestation of humility and submission.<br><br>
+Yet, such reverence cannot be fulfilled through mere outward actions or physical gestures. To bow or prostrate with one’s forehead touching the earth is not, in itself, the embodiment of true humility or modesty. No action we perform can be deemed worship unless it is executed in accordance with the sacred ordinances of Allah.<br><br>
+The Almighty has instructed us to worship him in a way that draws us near to Him, with no veils creating distance or detachment. This closeness can only be attained when the pure light of Tawheed is engraved deeply within the soul.<br><br>
+Sadly, the worship we witness today—cloaked in the guise of devotion—too often strays from this divine path. Even in the noble act of du’a, among the highest forms of worship, our hearts wander. We utter “Ameen” absent-mindedly, without presence, understanding or intention, to foreign phrases recited by someone else.<br><br>
+Our souls—the very place upon which Allah gazes—are devoid of His remembrance. Tawheed is absent within. How then can we claim that these du’as are worthy acts, accepted by the Most Merciful?<br><br>
+Indeed, even salat—the greatest form of physical worship—has been reduced to hollow gestures and sounds, stripped of its soul and its power to bring us closer to Allah.<br><br>
+Whether virtues or rituals, the Prophet ﷺ commanded his noble companions to perform them sincerely, by instituting the Tawheed in the souls - so that Allah would accept swiftly. The true heirs of the Prophet’s legacy—the righteous saints and scholars—have called us to the same path.<br><br>
+In our era, the guide of the age, Qutb al-Zaman Yousuf Sulthan, urged us to sanctify every moment of our lives as true worship. It is in the sacred mission of sharing this timeless message that he granted me the mantle of Khilafah.<br><br>
+Let us strive together to make the true Deen—the Deen entrusted to us by the Prophet ﷺ, his noble companions, and the virtuous like Shaykh Abdul Qadir Jeelani and Qutb al-Zaman—a living reality in our lives. <br><br>
+May Allah, the most Merciful, guide us to sincere worship, revive Tawheed in our hearts, and accept our efforts in His path. Ameen.
+    `}};export{e as q};
