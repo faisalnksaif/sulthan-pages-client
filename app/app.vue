c96ca29807a5eeda9414410483c67a7895b2b0c8
Faisal <faisalnkpadi@gmail.com>
2025-09-02 21:36:11 -0400

    Add meta

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,19 @@
     <Footer />
   </v-app>
 </template>
+<script setup>
+useHead({
+  script: [
+    {
+      type: "application/ld+json",
+      children: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Sulthan Pages",
+        "url": "https://sulthanpages.com",
+        "logo": "https://sulthanpages.com/logo.png"
+      })
+    }
+  ]
+})
+</script>
